6c89d2e318d7a5836f40669427d815d23501202b
Marcos-Sobral <vini15oficial@gmail.com>
2024-11-30 17:56:06 -0300

    ♻️: S1: refactor: refatorando Page inicio

--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -7,31 +7,31 @@ import imagem4 from '../../assets/image4.png';
 
 export default function Inicio() {
     return(
-        <main>
+        <>
             <section className={s.primeiraSessaoInicio}>
                 <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
             </section>
             <section className={s.porqueDoarSection}>
                 <h2>Por que devo doar?</h2>
                 <div className={s.containerCards}>
-                    <section>
+                    <section className="">
                         <img src={imagem1} alt="Imagem ilustrando círculo social de 4 pessoas" />
                         <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
                     </section>
-                    <section>
+                    <section className="">
                         <img src={imagem2} alt="Imagem ilustrando pessoa lendo" />
                         <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
                     </section>
-                    <section>
+                    <section className="">
                         <img src={imagem3} alt="Imagem ilustrando 6 pessoas transformando o mundo" />
                         <p>Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.</p>
                     </section>
-                    <section>
+                    <section className="">
                         <img src={imagem4} alt="Imagem ilustrando balança que representa igualdade" />
                         <p>Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>
                     </section>
                 </div>
             </section>
-        </main>
+        </>
     ) 
 }
