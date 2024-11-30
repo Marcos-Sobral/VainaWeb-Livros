e4063e90602c004d53e5e1956e9ddde44b7fbb8c
Marcos-Sobral <vini15oficial@gmail.com>
2024-11-30 18:36:03 -0300

    ♻️: S1: refactor: refatorando page de doacao

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -3,11 +3,28 @@ import s from './livrosDoados.module.scss'
 
 export default function LivrosDoados() {
     return(
+        <>
         <section className={s.LivrosDoadosSection}>
             <h2>Livros Doados</h2>
             <section className={s.containerCards}>
                 <section>
-                    <img src={livroProtagonista} alt="" />
+                    <img src={livroProtagonista} alt="capa do livro O protagonista, escrito por Sussane Andrade" />
+                    <div>
+                        <h3>O protagonista</h3>
+                        <p>Susanne Andrade</p>
+                        <p>Ficção</p>
+                    </div>
+                </section>
+                <section>
+                    <img src={livroProtagonista} alt="capa do livro O protagonista, escrito por Sussane Andrade" />
+                    <div>
+                        <h3>O protagonista</h3>
+                        <p>Susanne Andrade</p>
+                        <p>Ficção</p>
+                    </div>
+                </section>
+                <section>
+                    <img src={livroProtagonista} alt="capa do livro O protagonista, escrito por Sussane Andrade" />
                     <div>
                         <h3>O protagonista</h3>
                         <p>Susanne Andrade</p>
@@ -16,6 +33,8 @@ export default function LivrosDoados() {
                 </section>
             </section>
         </section>
+       
+        </>
     )
     
 }
