5d9b72e4f49a91cf791e84626eb53a8909cac8af
Marcos-Sobral <vini15oficial@gmail.com>
2025-03-28 16:10:04 -0300

    Add API call to retrieve donated books

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,11 +1,27 @@
+import axios from 'axios';
 import livroProtagonista from  '../../assets/book.png'
 import s from './livrosDoados.module.scss'
 
 export default function LivrosDoados() {
+
+    const pegarDados = async() => {
+        let titulo, categoria, autor, image_url;
+
+        const endPointApi = "https://vnw-api.onrender.com/livrosdoados";
+        const resposta = await axios.get(endPointApi);
+        resposta.data.forEach(i => {
+           autor = i["autor"];
+           titulo = i["titulo"];
+           categoria = i["categoria"];
+           image_url = i["image_url"];
+        });
+        console.log(autor);
+    }
     return(
         <>
         <section className={s.LivrosDoadosSection}>
             <h2>Livros Doados</h2>
+            <button onClick={pegarDados}>OI</button>
             <section className={s.containerCards}>
                 <section>
                     <img src={livroProtagonista} alt="capa do livro O protagonista, escrito por Sussane Andrade" />
