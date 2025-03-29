461acd90b1be0daa4c32dde438a64fc106193ab8
Marcos-Sobral <vini15oficial@gmail.com>
2025-03-29 12:29:48 -0300

    Refactor LivrosDoados component and update styles

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,52 +1,78 @@
 import axios from 'axios';
 import livroProtagonista from  '../../assets/book.png'
 import s from './livrosDoados.module.scss'
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 
 export default function LivrosDoados() {
 
-    const pegarDados = async() => {
-        let titulo, categoria, autor, image_url;
-
+        const [titulo, setTitulo] = useState([]);
+        const [categoria, setCategoria] = useState([]);
+        const [autor, setAutor] = useState([]);
+        const [image_url, setImage_url] = useState([]);
+        
         const endPointApi = "https://vnw-api.onrender.com/livrosdoados";
-        const resposta = await axios.get(endPointApi);
-        resposta.data.forEach(i => {
-           autor = i["autor"];
-           titulo = i["titulo"];
-           categoria = i["categoria"];
-           image_url = i["image_url"];
-        });
-        console.log(autor);
-    }
+
+        const pegarDados = async() =>{
+            try {                
+                const dados = {
+                    titulo,
+                    categoria,
+                    autor,
+                    image_url
+                }
+    
+                const resposta = await axios.get(endPointApi,dados);
+                let array = resposta.data;
+                console.log("Dados recebidos com sucesso!", array);
+                
+                let section =  window.document.getElementById("conteudo");
+                section.innerHTML = resposta.data.map(i =>
+                    `
+                    <section>
+                        <img src=${i[`image_url`]} />
+                        <div>
+                            <h3>${i[`titulo`]}</h3>
+                            <p>${i[`autor`]}</p>
+                            <p>${i[`categoria`]}</p>
+                        </div>
+                    </section>
+                `
+                );
+
+                /*resposta.data.forEach(i => {
+                    setTitulo(i["titulo"]);
+                    setCategoria(i["categoria"]);
+                    setAutor(i["autor"]);
+                    console.log(i["autor"]);
+                    setImage_url(i["image_url"]);
+                    section.innerHTML = `
+                        <section>
+                            <img src=${image_url} />
+                            <div>
+                                <h3>${titulo}</h3>
+                                <p>${autor}</p>
+                                <p>${categoria}</p>
+                            </div>
+                        </section>
+                    `
+                });*/
+            } catch (error) {
+                console.log("Ocorreu algum erro no recebimento dos dados: ", error);
+            }
+        }
+            
+        //carregamento automatico da função ao carregar ou atualizar a pg
+        useEffect(()=>{
+            pegarDados();
+        },[]);
     return(
         <>
         <section className={s.LivrosDoadosSection}>
             <h2>Livros Doados</h2>
-            <button onClick={pegarDados}>OI</button>
-            <section className={s.containerCards}>
-                <section>
-                    <img src={livroProtagonista} alt="capa do livro O protagonista, escrito por Sussane Andrade" />
-                    <div>
-                        <h3>O protagonista</h3>
-                        <p>Susanne Andrade</p>
-                        <p>Ficção</p>
-                    </div>
-                </section>
-                <section>
-                    <img src={livroProtagonista} alt="capa do livro O protagonista, escrito por Sussane Andrade" />
-                    <div>
-                        <h3>O protagonista</h3>
-                        <p>Susanne Andrade</p>
-                        <p>Ficção</p>
-                    </div>
-                </section>
-                <section>
-                    <img src={livroProtagonista} alt="capa do livro O protagonista, escrito por Sussane Andrade" />
-                    <div>
-                        <h3>O protagonista</h3>
-                        <p>Susanne Andrade</p>
-                        <p>Ficção</p>
-                    </div>
-                </section>
+            <section id="conteudo" className={s.containerCards}>
+
             </section>
         </section>
        
